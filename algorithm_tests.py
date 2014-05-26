44b1925f08a8b19611a8e1be848a3b4c067674bf
kjrz <kuba.jarz@gmail.com>
2014-05-26 23:45:52 +0200

    added for None situations in is_compact function

--- algorithm_tests.py
+++ algorithm_tests.py
@@ -7,7 +7,18 @@ class TestIsCompactFunction(unittest.TestCase):
         self.assertTrue(algorithm.is_compact({}))
 
     def test_one_element_colouring_is_compact(self):
-        self.assertTrue(algorithm.is_compact({('ee', 'fee') : 0}))
+        self.assertTrue(algorithm.is_compact({('ee', 'fee'): 0}))
+
+    def test_only_none_elements(self):
+        self.assertTrue(algorithm.is_compact({('ee'): None, ('eb'): None}))
+    
+    def test_some_none_elements(self):
+        self.assertTrue(algorithm.is_compact({('ee'): None, ('eb'): None, \
+                                              ('ec'): 4, ('ed'): 3}))
+    
+    def test_some_none_elements_not_compact(self):
+        self.assertFalse(algorithm.is_compact({('ee'): None, ('eb'): None, \
+                                              ('ec'): 4, ('ed'): 1}))
 
 if __name__ == '__main__':
     unittest.main()
