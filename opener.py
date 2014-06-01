26dc353fead48195360da6829b50047e4ad44680
kjrz <kuba.jarz@gmail.com>
2014-06-01 13:48:36 +0200

    logging added

--- opener.py
+++ opener.py
@@ -1,9 +1,15 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 import networkx as nx
+from pprint import pformat
 
 def read_edges_from_file(path):
     graph_file = open(path)
     lines = graph_file.readlines()
     edges = [tuple(line.split()) for line in lines]
+    logger.info("Graph read from file:\n%s", pformat(edges))
     return edges
 
 def read_graph_from_file(path):
