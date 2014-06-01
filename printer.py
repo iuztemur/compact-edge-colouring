8dfc6b13cc1354330a5a761bcb577a56bf6933e2
kjrz <kuba.jarz@gmail.com>
2014-06-01 13:53:33 +0200

    colouring to file printer

---
+++ printer.py
@@ -0,0 +1,14 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+import pprint
+
+def print_colouring(colouring, colouring_file):
+    logger.info("Writing colouring to file '%s'", colouring_file)
+    f = open(colouring_file, 'w')
+
+    for key in colouring:
+        f.write(key[0] + ' ' + key[1] + ' ' + str(colouring[key]) + '\n')
+
+    f.close()
